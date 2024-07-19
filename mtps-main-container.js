5505914d43a9dc8ac40aefb96208563dd9363f2d
Yashkumar Rameshbhai Trada <yashtrada1@gmail.com>
2024-07-19 18:02:25 -0300

    Update mtps-main-container.js

--- mtps-main-container.js
+++ mtps-main-container.js
@@ -1,20 +1,24 @@
-In the below Node JS code, I want to store the total number of rows in data in rowCounter variable.
+let rowCounter = 0;
 
 data.forEach((row, rowIdx) => {
-			let colIdx = 0
-			let lengthFlag = false
-			exceptionoccuredRow = rowIdx
+    rowCounter++; // increment the counter for each row
 
-			//mapping column headers between schema & excel sheet
-			let mapping_ColIdx = 0;
-			let mappedRow = null
-			_.forEach(row, (rowVal, col) => {
+    let colIdx = 0;
+    let lengthFlag = false;
+    exceptionoccuredRow = rowIdx;
 
-				let testCol = schema[mapping_ColIdx].columnName
-				mappedRow = {
-					...mappedRow,
-					[testCol]: rowVal
-				}
-				mapping_ColIdx++;
-			})			
-}
+    //mapping column headers between schema & excel sheet
+    let mapping_ColIdx = 0;
+    let mappedRow = null;
+    _.forEach(row, (rowVal, col) => {
+
+        let testCol = schema[mapping_ColIdx].columnName;
+        mappedRow = {
+            ...mappedRow,
+            [testCol]: rowVal
+        };
+        mapping_ColIdx++;
+    });			
+});
+
+console.log(`Total number of rows: ${rowCounter}`);
