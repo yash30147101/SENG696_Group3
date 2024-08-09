07153e152cbfc05963d9fd900585edf6b22cda7f
Yashkumar Rameshbhai Trada <yashtrada1@gmail.com>
2024-08-09 15:02:16 -0300

    Update mtps-main-container.sh

--- mtps-main-container.sh
+++ mtps-main-container.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-# Define the source directory
-SRCDIR="${APP_HOME}/data/recon/$FROM/$SRC/$SRCID/$TYPE"
+# Define the file path
+FILE="Folder_Path/FileName"
 
-# Get the current year and month
-YYYYMM=$(date +%Y%m)
-
-# Rename PensionHistoryReport files
-for file in "$SRCDIR"/PensionHistoryReport*.csv; do
-  if [[ -f "$file" ]]; then
-    mv "$file" "${SRCDIR}/PensionHistoryReport${YYYYMM}01.csv"
-  fi
-done
-
-# Rename RecipientHistoryReport files
-for file in "$SRCDIR"/RecipientHistoryReport*.csv; do
-  if [[ -f "$file" ]]; then
-    mv "$file" "${SRCDIR}/RecipientHistoryReport${YYYYMM}01.csv"
-  fi
-done
+# Check if the file exists
+if [ -f "$FILE" ]; then
+    # If the file exists, delete it
+    rm "$FILE"
